ab66a7fc3e46018738cabcd19a13c70da7c57253
Gerard Paligot <gerard.paligot@gmail.com>
2022-07-12 00:07:01 +0200

    refactor(switches): Apply new compose contract convention.

--- switches/src/main/java/com/decathlon/vitamin/compose/switches/VitaminSwitches.kt
+++ switches/src/main/java/com/decathlon/vitamin/compose/switches/VitaminSwitches.kt
@@ -2,6 +2,7 @@ package com.decathlon.vitamin.compose.switches
 
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.material.Switch
+import androidx.compose.material.SwitchColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -9,20 +10,30 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.decathlon.vitamin.compose.foundation.VitaminTheme
 
 object VitaminSwitches {
+    /**
+     * The primary switch toggle the state of a single item on or off.
+     * @param checked Whether Switch is checked or unchecked
+     * @param modifier The [Modifier] to be applied to the component
+     * @param enabled True if you can check the switch, otherwise false
+     * @param interactionSource Representing the stream of interaction for the switch
+     * @param colors The colors of the switch in selected and unselected mode
+     * @param onCheckedChange The callback to be called when the state of the switch changed
+     */
     @Composable
     fun Primary(
         checked: Boolean,
         modifier: Modifier = Modifier,
-        onCheckedChange: ((Boolean) -> Unit)? = null,
         enabled: Boolean = true,
-        interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+        interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+        colors: SwitchColors = VitaminSwitchesColors.primary(),
+        onCheckedChange: ((Boolean) -> Unit)? = null
     ) {
         Switch(
             checked = checked,
             modifier = modifier,
             onCheckedChange = onCheckedChange,
             enabled = enabled,
-            colors = VitaminSwitchesColors.primary,
+            colors = colors,
             interactionSource = interactionSource
         )
     }
