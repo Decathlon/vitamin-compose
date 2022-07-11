099d764b7e2fe113e3a5ec3c1db83d3f11d910d2
Gerard Paligot <gerard.paligot@gmail.com>
2022-07-11 17:30:29 +0200

    refactor(menus): Apply new compose contract convention.

---
+++ menus/src/main/java/com/decathlon/vitamin/compose/dropdown/MenuItemColors.kt
@@ -0,0 +1,11 @@
+package com.decathlon.vitamin.compose.dropdown
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+
+@Immutable
+class MenuItemColors(
+    val background: Color,
+    val contentColor: Color,
+    val iconColor: Color
+)
