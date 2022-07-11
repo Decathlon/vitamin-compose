17eb649de306b8a1a0c9cf02289d92355ad50db6
Gerard Paligot <gerard.paligot@gmail.com>
2022-07-12 00:07:01 +0200

    refactor(switches): Apply new compose contract convention.

--- switches/README.md
+++ switches/README.md
@@ -21,9 +21,10 @@ object VitaminSwitches {
     fun Primary(
         checked: Boolean,
         modifier: Modifier = Modifier,
-        onCheckedChange: ((Boolean) -> Unit)? = null,
         enabled: Boolean = true,
-        interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+        interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+        colors: SwitchColors = VitaminSwitchesColors.primary(),
+        onCheckedChange: ((Boolean) -> Unit)? = null
     )
 }
 ```
@@ -48,6 +49,7 @@ Parameters | Descriptions
 -- | --
 `checked: Boolean` | Whether Switch is checked or unchecked
 `modifier: Modifier = Modifier` | The `Modifier` to be applied to the component
-`onCheckedChange: ((Boolean) -> Unit)? = null` | The callback to be called when the state of the switch changed
 `enabled: Boolean = true` | True if you can check the switch, otherwise false
 `interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }` | Representing the stream of interaction for the switch
+`colors: SwitchColors = VitaminSwitchesColors.primary()` | The colors of the switch in selected and unselected mode
+`onCheckedChange: ((Boolean) -> Unit)? = null` | The callback to be called when the state of the switch changed
--- switches/src/main/java/com/decathlon/vitamin/compose/switches/VitaminSwitchesColors.kt
+++ switches/src/main/java/com/decathlon/vitamin/compose/switches/VitaminSwitchesColors.kt
@@ -3,31 +3,42 @@ package com.decathlon.vitamin.compose.switches
 import androidx.compose.material.SwitchColors
 import androidx.compose.material.SwitchDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.compositeOver
 import com.decathlon.vitamin.compose.foundation.VitaminTheme
 import com.decathlon.vitamin.compose.foundation.VtmnStatesDisabled
 
-internal object VitaminSwitchesColors {
-    val primary: SwitchColors
-        @Composable
-        get() = SwitchDefaults.colors(
-            checkedThumbColor = VitaminTheme.colors.vtmnContentActive,
-            checkedTrackColor = VitaminTheme.colors.vtmnContentActive,
-            checkedTrackAlpha = 0.54f,
-            uncheckedThumbColor = VitaminTheme.colors.vtmnContentPrimaryReversed,
-            uncheckedTrackColor = VitaminTheme.colors.vtmnContentInactive,
-            uncheckedTrackAlpha = 1f,
-            disabledCheckedThumbColor = VitaminTheme.colors.vtmnContentActive
-                .copy(alpha = VtmnStatesDisabled)
-                .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed),
-            disabledCheckedTrackColor = VitaminTheme.colors.vtmnContentActive
-                .copy(alpha = VtmnStatesDisabled)
-                .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed),
-            disabledUncheckedThumbColor = VitaminTheme.colors.vtmnContentPrimaryReversed
-                .copy(alpha = VtmnStatesDisabled)
-                .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed),
-            disabledUncheckedTrackColor = VitaminTheme.colors.vtmnContentInactive
-                .copy(alpha = VtmnStatesDisabled)
-                .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed)
-        )
+object VitaminSwitchesColors {
+    @Composable
+    fun primary(
+        checkedThumbColor: Color = VitaminTheme.colors.vtmnContentActive,
+        checkedTrackColor: Color = VitaminTheme.colors.vtmnContentActive,
+        checkedTrackAlpha: Float = 0.54f,
+        uncheckedThumbColor: Color = VitaminTheme.colors.vtmnContentPrimaryReversed,
+        uncheckedTrackColor: Color = VitaminTheme.colors.vtmnContentInactive,
+        uncheckedTrackAlpha: Float = 1f,
+        disabledCheckedThumbColor: Color = VitaminTheme.colors.vtmnContentActive
+            .copy(alpha = VtmnStatesDisabled)
+            .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed),
+        disabledCheckedTrackColor: Color = VitaminTheme.colors.vtmnContentActive
+            .copy(alpha = VtmnStatesDisabled)
+            .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed),
+        disabledUncheckedThumbColor: Color = VitaminTheme.colors.vtmnContentPrimaryReversed
+            .copy(alpha = VtmnStatesDisabled)
+            .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed),
+        disabledUncheckedTrackColor: Color = VitaminTheme.colors.vtmnContentInactive
+            .copy(alpha = VtmnStatesDisabled)
+            .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed)
+    ): SwitchColors = SwitchDefaults.colors(
+        checkedThumbColor = checkedThumbColor,
+        checkedTrackColor = checkedTrackColor,
+        checkedTrackAlpha = checkedTrackAlpha,
+        uncheckedThumbColor = uncheckedThumbColor,
+        uncheckedTrackColor = uncheckedTrackColor,
+        uncheckedTrackAlpha = uncheckedTrackAlpha,
+        disabledCheckedThumbColor = disabledCheckedThumbColor,
+        disabledCheckedTrackColor = disabledCheckedTrackColor,
+        disabledUncheckedThumbColor = disabledUncheckedThumbColor,
+        disabledUncheckedTrackColor = disabledUncheckedTrackColor
+    )
 }
