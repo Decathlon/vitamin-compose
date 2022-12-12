58d28c2e4c40875addaafc13dca477b68f378231
ManonPolle <manon.polle@decathlon.com>
2022-12-12 11:35:24 +0100

    feat(vitamin): Add Vitamin Module

---
+++ vitamin/README.md
@@ -0,0 +1,17 @@
+# Vitamin module
+
+## Responsibilities
+
+This module provides all components modules.  
+
+## How to use it?
+
+If you want to use all of Vitamin components, you can import this dependency in your `build.gradle` file of your
+Android project:
+
+```kotlin
+implementation("com.decathlon.vitamin.compose:vitamin:<version>")
+```
+
+If you want to know how to use `VitaminTheme` please read the [Foundation README](../foundation/README.md) 
+
---
+++ vitamin/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("VitaminComposeLibraryPlugin")
+    id("com.vanniktech.maven.publish")
+}
+
+dependencies {
+    api(project(":appbars"))
+    api(project(":badges"))
+    api(project(":buttons"))
+    api(project(":checkboxes"))
+    api(project(":dividers"))
+    api(project(":foundation"))
+    api(project(":menus"))
+    api(project(":modals"))
+    api(project(":prices"))
+    api(project(":progressbars"))
+    api(project(":radiobuttons"))
+    api(project(":ratings"))
+    api(project(":skeletons"))
+    api(project(":snackbars"))
+    api(project(":switches"))
+    api(project(":tabs"))
+    api(project(":tags"))
+    api(project(":text-inputs"))
+}
