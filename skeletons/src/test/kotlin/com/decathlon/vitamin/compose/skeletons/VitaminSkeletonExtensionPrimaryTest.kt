72ca63840c740369b88739deab455a7336ea77da
FLORIAN <florian.delahaye@decathlon.com>
2023-03-03 16:13:59 +0100

    chore(skeletons): add paparazzi tests

--- skeletons/src/test/kotlin/com/decathlon/vitamin/compose/skeletons/VitaminSkeletonExtensionPrimaryTest.kt
+++ skeletons/src/test/kotlin/com/decathlon/vitamin/compose/skeletons/VitaminSkeletonExtensionPrimaryTest.kt
@@ -1,18 +1,20 @@
-package com.decathlon.vitamin.compose.dividers
+package com.decathlon.vitamin.compose.skeletons
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import app.cash.paparazzi.Paparazzi
-import com.decathlon.vitamin.compose.dividers.utils.DividerVariantsFactory
-import com.decathlon.vitamin.compose.dividers.utils.Theme
-import com.decathlon.vitamin.compose.dividers.utils.Variant
 import com.decathlon.vitamin.compose.foundation.VitaminTheme
+import com.decathlon.vitamin.compose.skeletons.utils.SkeletonVariantsFactory
+import com.decathlon.vitamin.compose.skeletons.utils.Theme
+import com.decathlon.vitamin.compose.skeletons.utils.Variant
 import com.google.testing.junit.testparameterinjector.TestParameter
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import org.junit.Rule
@@ -21,15 +23,14 @@ import org.junit.runner.RunWith
 
 
 @RunWith(TestParameterInjector::class)
-class VitaminDividersPrimaryTest(
+class VitaminSkeletonExtensionPrimaryTest(
     @TestParameter val variant: Variant
 ) {
     @get:Rule
     val paparazzi = Paparazzi()
 
-    @Suppress("LongMethod")
     @Test
-    fun medium(@TestParameter theme: Theme) {
+    fun extension(@TestParameter theme: Theme) {
         paparazzi.snapshot {
             VitaminTheme(theme == Theme.Dark) {
                 Scaffold { padding ->
@@ -37,9 +38,15 @@ class VitaminDividersPrimaryTest(
                         modifier = Modifier.padding(padding),
                         verticalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
-                        Text(text = variant.name, style = VitaminTheme.typography.subtitle1)
-                        DividerVariantsFactory.Divider(
-                            modifier = Modifier.fillMaxWidth(), variant = variant
+                        Text(
+                            text = "Extension ${variant.name}",
+                            style = VitaminTheme.typography.subtitle1
+                        )
+                        SkeletonVariantsFactory.SkeletonExtension(
+                            variant = variant,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(30.dp)
                         )
                     }
                 }
---
+++ skeletons/src/test/kotlin/com/decathlon/vitamin/compose/skeletons/utils/Variant.kt
@@ -0,0 +1,5 @@
+package com.decathlon.vitamin.compose.skeletons.utils
+
+enum class Variant {
+    Rectangular, Rounded
+}
