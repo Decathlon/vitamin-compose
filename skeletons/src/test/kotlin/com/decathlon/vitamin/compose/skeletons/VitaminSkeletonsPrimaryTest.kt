a3148b6c4ee2767c7e7ed6edc18b13cdc5bbc72f
FLORIAN <florian.delahaye@decathlon.com>
2023-03-03 16:13:59 +0100

    chore(skeletons): add paparazzi tests

--- skeletons/src/test/kotlin/com/decathlon/vitamin/compose/skeletons/VitaminSkeletonsPrimaryTest.kt
+++ skeletons/src/test/kotlin/com/decathlon/vitamin/compose/skeletons/VitaminSkeletonsPrimaryTest.kt
@@ -1,18 +1,20 @@
-package com.decathlon.vitamin.compose.dividers
+package com.decathlon.vitamin.compose.skeletons
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import app.cash.paparazzi.Paparazzi
-import com.decathlon.vitamin.compose.dividers.utils.DividerVariantsFactory
-import com.decathlon.vitamin.compose.dividers.utils.Theme
-import com.decathlon.vitamin.compose.dividers.utils.Variant
 import com.decathlon.vitamin.compose.foundation.VitaminTheme
+import com.decathlon.vitamin.compose.skeletons.utils.SkeletonVariantsFactory
+import com.decathlon.vitamin.compose.skeletons.utils.Theme
+import com.decathlon.vitamin.compose.skeletons.utils.Variant
 import com.google.testing.junit.testparameterinjector.TestParameter
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import org.junit.Rule
@@ -21,13 +23,12 @@ import org.junit.runner.RunWith
 
 
 @RunWith(TestParameterInjector::class)
-class VitaminDividersPrimaryTest(
+class VitaminSkeletonsPrimaryTest(
     @TestParameter val variant: Variant
 ) {
     @get:Rule
     val paparazzi = Paparazzi()
 
-    @Suppress("LongMethod")
     @Test
     fun medium(@TestParameter theme: Theme) {
         paparazzi.snapshot {
@@ -38,8 +39,8 @@ class VitaminDividersPrimaryTest(
                         verticalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
                         Text(text = variant.name, style = VitaminTheme.typography.subtitle1)
-                        DividerVariantsFactory.Divider(
-                            modifier = Modifier.fillMaxWidth(), variant = variant
+                        SkeletonVariantsFactory.Skeleton(
+                            modifier = Modifier.fillMaxWidth().height(30.dp), variant = variant
                         )
                     }
                 }
