dae2ee08767fe32ca27241503d8d781cd7c60216
ManonPolle <manon.polle@decathlon.com>
2023-05-17 09:57:27 +0200

    fix(switches): Fix thumb and track colors in dark mode

--- switches/src/main/java/com/decathlon/vitamin/compose/switches/VitaminSwitchesColors.kt
+++ switches/src/main/java/com/decathlon/vitamin/compose/switches/VitaminSwitchesColors.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.SwitchDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.compositeOver
+import com.decathlon.vitamin.compose.foundation.VitaminPalette
 import com.decathlon.vitamin.compose.foundation.VitaminTheme
 import com.decathlon.vitamin.compose.foundation.VtmnStatesDisabled
 
@@ -12,21 +13,21 @@ object VitaminSwitchesColors {
     @Composable
     fun primary(
         checkedThumbColor: Color = VitaminTheme.colors.vtmnContentActive,
-        checkedTrackColor: Color = VitaminTheme.colors.vtmnContentActive,
+        checkedTrackColor: Color = VitaminPalette.vtmnBlue200,
         checkedTrackAlpha: Float = 0.54f,
-        uncheckedThumbColor: Color = VitaminTheme.colors.vtmnContentPrimaryReversed,
+        uncheckedThumbColor: Color = VitaminPalette.vtmnWhite,
         uncheckedTrackColor: Color = VitaminTheme.colors.vtmnContentInactive,
         uncheckedTrackAlpha: Float = 1f,
-        disabledCheckedThumbColor: Color = VitaminTheme.colors.vtmnContentActive
+        disabledCheckedThumbColor: Color = checkedThumbColor
             .copy(alpha = VtmnStatesDisabled)
             .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed),
-        disabledCheckedTrackColor: Color = VitaminTheme.colors.vtmnContentActive
+        disabledCheckedTrackColor: Color = checkedTrackColor
             .copy(alpha = VtmnStatesDisabled)
             .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed),
-        disabledUncheckedThumbColor: Color = VitaminTheme.colors.vtmnContentPrimaryReversed
+        disabledUncheckedThumbColor: Color = uncheckedThumbColor
             .copy(alpha = VtmnStatesDisabled)
             .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed),
-        disabledUncheckedTrackColor: Color = VitaminTheme.colors.vtmnContentInactive
+        disabledUncheckedTrackColor: Color = uncheckedTrackColor
             .copy(alpha = VtmnStatesDisabled)
             .compositeOver(VitaminTheme.colors.vtmnContentPrimaryReversed)
     ): SwitchColors = SwitchDefaults.colors(
