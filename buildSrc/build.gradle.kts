8b4344083a2ec693026a2c799ad87e58fe9f4f03
FLORIAN <florian.delahaye@decathlon.com>
2023-06-16 16:14:14 +0200

    fix(progressbars): add rounded corner to progress bars

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -36,5 +36,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
     implementation("com.vanniktech:gradle-maven-publish-plugin:0.18.0")
     implementation("app.cash.licensee:licensee-gradle-plugin:1.4.1")
-    implementation("app.cash.paparazzi:paparazzi-gradle-plugin:1.2.0")
+    implementation("app.cash.paparazzi:paparazzi-gradle-plugin:1.3.0")
 }
