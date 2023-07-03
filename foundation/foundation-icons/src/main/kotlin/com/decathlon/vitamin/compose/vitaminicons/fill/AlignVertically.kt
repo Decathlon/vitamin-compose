cc633095b27e13f98c39155af8702dbc47f08818
lauthieb <laurent.thiebault@decathlon.com>
2023-07-03 22:56:24 +0000

    Update from https://github.com/Decathlon/vitamin-design/commit/8c52d414c40af4d99a5f0cfb4d0232e051111159

---
+++ foundation/foundation-icons/src/main/kotlin/com/decathlon/vitamin/compose/vitaminicons/fill/AlignVertically.kt
@@ -0,0 +1,72 @@
+package com.decathlon.vitamin.compose.vitaminicons.fill
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import com.decathlon.vitamin.compose.vitaminicons.FillGroup
+
+public val FillGroup.AlignVertically: ImageVector
+    get() {
+        if (_alignVertically != null) {
+            return _alignVertically!!
+        }
+        _alignVertically = Builder(name = "AlignVertically", defaultWidth = 24.0.dp, defaultHeight =
+                24.0.dp, viewportWidth = 24.0f, viewportHeight = 24.0f).apply {
+            path(fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(3.0f, 11.0f)
+                horizontalLineTo(21.0f)
+                verticalLineTo(13.0f)
+                horizontalLineTo(3.0f)
+                verticalLineTo(11.0f)
+                close()
+                moveTo(18.0f, 18.0f)
+                verticalLineTo(21.0f)
+                horizontalLineTo(16.0f)
+                verticalLineTo(18.0f)
+                horizontalLineTo(13.0f)
+                lineTo(17.0f, 14.0f)
+                lineTo(21.0f, 18.0f)
+                horizontalLineTo(18.0f)
+                close()
+                moveTo(8.0f, 18.0f)
+                verticalLineTo(21.0f)
+                horizontalLineTo(6.0f)
+                verticalLineTo(18.0f)
+                horizontalLineTo(3.0f)
+                lineTo(7.0f, 14.0f)
+                lineTo(11.0f, 18.0f)
+                horizontalLineTo(8.0f)
+                close()
+                moveTo(18.0f, 6.0f)
+                horizontalLineTo(21.0f)
+                lineTo(17.0f, 10.0f)
+                lineTo(13.0f, 6.0f)
+                horizontalLineTo(16.0f)
+                verticalLineTo(3.0f)
+                horizontalLineTo(18.0f)
+                verticalLineTo(6.0f)
+                close()
+                moveTo(8.0f, 6.0f)
+                horizontalLineTo(11.0f)
+                lineTo(7.0f, 10.0f)
+                lineTo(3.0f, 6.0f)
+                horizontalLineTo(6.0f)
+                verticalLineTo(3.0f)
+                horizontalLineTo(8.0f)
+                verticalLineTo(6.0f)
+                close()
+            }
+        }
+        .build()
+        return _alignVertically!!
+    }
+
+private var _alignVertically: ImageVector? = null
---
+++ foundation/foundation-icons/src/main/kotlin/com/decathlon/vitamin/compose/vitaminicons/line/Text.kt
@@ -0,0 +1,40 @@
+package com.decathlon.vitamin.compose.vitaminicons.line
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import com.decathlon.vitamin.compose.vitaminicons.LineGroup
+
+public val LineGroup.Text: ImageVector
+    get() {
+        if (_text != null) {
+            return _text!!
+        }
+        _text = Builder(name = "Text", defaultWidth = 24.0.dp, defaultHeight = 24.0.dp,
+                viewportWidth = 24.0f, viewportHeight = 24.0f).apply {
+            path(fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(13.0f, 6.0f)
+                verticalLineTo(21.0f)
+                horizontalLineTo(11.0f)
+                verticalLineTo(6.0f)
+                horizontalLineTo(5.0f)
+                verticalLineTo(4.0f)
+                horizontalLineTo(19.0f)
+                verticalLineTo(6.0f)
+                horizontalLineTo(13.0f)
+                close()
+            }
+        }
+        .build()
+        return _text!!
+    }
+
+private var _text: ImageVector? = null
